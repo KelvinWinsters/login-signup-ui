477e500117b2d2015919b15fb0a887a91941d644
KelvinWinsters <kelvinwinsters@gmail.com>
2020-10-02 15:25:52 +0300

    login ui done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,177 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: new MyHomePage(),
+    );
+  }
+}
 
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      resizeToAvoidBottomPadding: false,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
+                  child: Text(
+                    'Hello',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 80),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(15.0, 200.0, 0.0, 0.0),
+                  child: Text(
+                    'World',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 80),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(250.0, 200.0, 0.0, 0.0),
+                  child: Text(
+                    '.',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 80,
+                        color: Colors.lightBlue),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
+            child: Column(
+              children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(
+                      labelText: "Email",
+                      labelStyle: TextStyle(
+                          fontFamily: "",
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey)),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                      labelText: "Password",
+                      labelStyle: TextStyle(
+                          fontFamily: "",
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey)),
+                  obscureText: true,
+                ),
+                SizedBox(
+                  height: 5.0,
+                ),
+                Container(
+                  alignment: Alignment(1.0, 0.0),
+                  padding: EdgeInsets.only(left: 20.0, top: 15.0),
+                  child: InkWell(
+                    child: Text(
+                      'Forgot Password',
+                      style: TextStyle(
+                          color: Colors.lightBlue,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16.0,
+                          decoration: TextDecoration.underline),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Container(
+                  height: 40.0,
+                  child: Material(
+                    borderRadius: BorderRadius.circular(20.0),
+                    shadowColor: Colors.lightBlueAccent,
+                    color: Colors.lightBlue,
+                    elevation: 7.0,
+                    child: GestureDetector(
+                      onTap: () {},
+                      child: Center(
+                        child: Text(
+                          'LogIn',
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Container(
+                  height: 40.0,
+                  color: Colors.transparent,
+                  child: Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.black,
+                          style: BorderStyle.solid,
+                          width: 1.0,
+                        ),
+                        color: Colors.transparent,
+                        borderRadius: BorderRadius.circular(20.0)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Center(
+                          child: Text(
+                            "Log In with Twitter",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(height: 15.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("New Here",
+                style: TextStyle(),),
+              SizedBox(width: 5.0,),
+              InkWell(
+                onTap: () {},
+                child: Text('Register',
+                  style: TextStyle(
+                    color: Colors.lightBlue,
+                    fontWeight: FontWeight.bold,
+                    decoration: TextDecoration.underline
+                  ),),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
